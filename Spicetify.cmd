81b97a20d9d7d715dc1085d2bafe678400fd8ebc
baikil <78218286+baikil@users.noreply.github.com>
2024-04-25 12:16:51 -0400

    Updated to version 02

--- Spicetify.cmd
+++ Spicetify.cmd
@@ -5,22 +5,27 @@
 ::----------------------------------------------::
 @echo off
 
-set "logo=echo [38;2;246;147;45m   ____     _         __  _ ___&echo [38;2;243;123;45m  / __/__  (_)______ / /_(_) _/_ __   __ _  ___  ___  ___  ___  ___ ____&echo [38;2;237;96;53m _\ \/ _ \/ / __/ -_) __/ / _/ // /  /  ' \/ _ `/ _ \/ _ `/ _ `/ -_) __/&echo [38;2;233;78;58m/___/ .__/_/\__/\__/\__/_/_/ \_, /  /_/_/_/\_,_/_//_/\_,_/\_, /\__/_/&echo [38;2;224;76;64m   /_/                      /___/                        /___/&echo [[0m"
+set "logo=echo [38;2;246;147;45m   ____     _         __  _ ___&echo [38;2;243;123;45m  / __/__  (_)______ / /_(_) _/_ __   __ _  ___  ___  ___  ___  ___ ____&echo [38;2;237;96;53m _\ \/ _ \/ / __/ -_) __/ / _/ // /  /  ' \/ _ `/ _ \/ _ `/ _ `/ -_) __/&echo [38;2;233;78;58m/___/ .__/_/\__/\__/\__/_/_/ \_, /  /_/_/_/\_,_/_//_/\_,_/\_, /\__/_/&echo [38;2;224;76;64m   /_/                      /___/                        /___/&echo [38;5;237mVersion 02[0m"
 
-:00
+:00 Main menu
 cls&title Spicetify manager&%logo%&echo.
 echo 1. Install Spicetify
 echo 2. Install Spicetify apps
 echo 3. Update/restore Spicetify
 echo 4. Uninstall Spicetify
-choice /n /c 1234
+echo 5. Exit
+choice /n /c 12345
 goto %errorlevel%0
+
+::--------
 
 :10 Install Spicetify
 cls&title Spicetify manager - Installing Spicetify&%logo%&echo.
 powershell -command "iwr -useb https://raw.githubusercontent.com/spicetify/spicetify-cli/master/install.ps1 | iex"
 powershell -command "iwr -useb https://raw.githubusercontent.com/spicetify/spicetify-marketplace/main/resources/install.ps1 | iex"
-exit
+echo.&title Spicetify manager - Done&pause&goto 00
+
+::--------
 
 :20 Install Spicetify apps
 cls&title Spicetify manager - Install Spicetify apps&%logo%&echo.
@@ -32,9 +37,10 @@ echo 2. Lyrics Plus
 ::echo 6. Playlist Combiner
 ::echo 7. Beat Saber
 echo ---------------------
-echo B. (^<-)
-choice /n /c 1234567b
+echo 8. (^<-)
+choice /n /c 12345678
 goto 2%errorlevel%
+
 :21 Spicetify Stats
 cls&title Spicetify manager - Installing Spicetify Stats&%logo%&echo.
 powershell -command "(New-Object System.Net.WebClient).DownloadFile('https://codeload.github.com/harbassan/spicetify-stats/zip/refs/heads/dist','%appdata%/spicetify/CustomApps/spicetify-stats-dist.zip')"
@@ -44,31 +50,42 @@ rd /s /q "%appdata%/spicetify/CustomApps/unzipped/"
 del /f /q "%appdata%/spicetify/CustomApps/spicetify-stats-dist.zip"
 powershell -command "spicetify config custom_apps stats"
 powershell -command "spicetify apply"
-pause
-goto 20
+echo.&title Spicetify manager - Done&pause&goto 20
+
 :22 Lyrics Plus
 powershell -command "spicetify config custom_apps lyrics-plus"
 powershell -command "spicetify apply"
-pause
-goto 20
-:23
-:24
-:25
-:26
-:27
-:28
+echo.&title Spicetify manager - Done&pause&goto 20
+
+::Not yet implemented - pause and go back to the :20 menu
+:23 Eternal Jukebox
+:24 Better Local Files
+:25 Name that tune
+:26 Playlist Combiner
+:27 Beat Saber
+pause&goto 20
+:28 Back to main menu
 goto 00
+
+::--------
 
 :30 Update/restore Spicetify
 cls&title Spicetify manager - Updating/restoring Spicetify&%logo%&echo.
 powershell -command "spicetify backup"
 powershell -command "spicetify update"
 powershell -command "spicetify restore backup apply"
-exit
+echo.&title Spicetify manager - Done&pause&goto 00
+
+::--------
 
 :40 Uninstall Spicetify
 cls&title Spicetify manager - Uninstalling Spicetify&%logo%&echo.
 powershell -command "spicetify restore"
 powershell -command "rmdir -r -fo $env:APPDATA\spicetify"
 powershell -command "rmdir -r -fo $env:LOCALAPPDATA\spicetify"
+echo.&title Spicetify manager - Done&pause&goto 00
+
+::--------
+
+:50 Exit
 exit
